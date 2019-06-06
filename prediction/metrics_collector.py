7816f33834eb08e4880b17d94d31275c3642c0fa
ikalyvas <i.kalyvas@modio.io>
2019-06-07 01:09:27 +0300

    Write to csv the cpu load, and calculate the average cpu load of all the vdus

--- prediction/metrics_collector.py
+++ prediction/metrics_collector.py
@@ -1,6 +1,9 @@
+import math
+
 import yaml
 import requests
 import time
+import csv
 
 
 class MetricCollector(object):
@@ -17,21 +20,31 @@ class MetricCollector(object):
         return prom_ip, prom_port
 
     def get_cpu_load(self):
-        while True:
-            response = requests.get(self.prom_url+"api/v1/query?query=osm_cpu_utilization")
-            print(response.status_code)
-            resp = response.json()
-            results = resp.get("data").get("result")
-            if results:
-                print(f"Got {len(results)} VDUs to get cpu load ")
-                for result in results:
-                    vdu = result.get("metric").get("vdu_name")
-                    cpu_load = result.get("value")[-1]
-                    timestamp = result.get("value")[0]
-                    print(f"{vdu} has {cpu_load} recorded at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))}")
-            time.sleep(10)
+        with open('metric_cpu.csv', newline='', mode='a') as f:
+            fieldnames = ["ID", "TIMESTAMP", "CPU_LOAD"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            id_ = 0
+            while True:
+                response = requests.get(self.prom_url+"api/v1/query?query=osm_cpu_utilization")
+                resp = response.json()
+                results = resp.get("data").get("result")
+                if results:
+                    print(f"Got {len(results)} VDUs to get cpu load ")
+                    total = 0
+                    for result in results:
+                        vdu = result.get("metric").get("vdu_name")
+                        cpu_load = result.get("value")[-1]
+                        timestamp = result.get("value")[0]
+                        print(f"{vdu} has {cpu_load} recorded at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))}")
+                        total += float(cpu_load)
+                    avg = total / len(results)
+                    writer.writerow({"ID": id_, "TIMESTAMP": timestamp, "CPU_LOAD": avg})
+                    f.flush()
+                    id_ += 1
 
 
+                time.sleep(1)
 
 if __name__ == '__main__':
     m = MetricCollector('prometheus_cfg.yaml')
