3ce1e3c52ad1016ccb3ea5b2843eb6ef7b31cbd6
ikalyvas <i.kalyvas@modio.io>
2019-06-21 02:06:30 +0300

    clean up old files

--- metrics_manager/metrics_collector/vnf_detector_api/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class VnfDetectorApiConfig(AppConfig):
-    name = 'vnf_detector_api'
--- metrics_manager/metrics_collector/vnf_detector_api/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import LayerViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-
-router.register('vnfs', LayerViewSet, base_name='vnfs')
-
-urlpatterns = router.urls
