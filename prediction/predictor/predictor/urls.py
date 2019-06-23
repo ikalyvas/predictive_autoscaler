c9ba9b9e6d0a597e375c02c6fd817f0ef5aebb9c
ikalyvas <i.kalyvas@modio.io>
2019-06-23 22:46:31 +0300

    Created Swagger to document the predictors API Go to localhost:8000/ and see the endpoints

--- prediction/predictor/predictor/urls.py
+++ prediction/predictor/predictor/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Predictors API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', schema_view),
     re_path(r'^api/', include(('predictors.urls', 'predictors'), namespace='predictors'))
 ]
